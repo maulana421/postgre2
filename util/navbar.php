1fb95c985091ca9473a7f582fbb4822a45b17329
maulana ibnu fajar <72007694+maulana421@users.noreply.github.com>
2022-03-25 15:47:02 +0700

    Update navbar.php

--- util/navbar.php
+++ util/navbar.php
@@ -1,8 +1,8 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-white p-3 px-md-3 py-md-3 fixed-top shadow sm">
         <a href="./index.php" class="navbar-brand">
-            <img src="./img/logo.png" width="30" height="30" alt="" class="d-inline-block align-top">
-            Nors Academy
+            <img src="./img/ji.png" width="30" height="30" alt="" class="d-inline-block align-top">
+            Gloxinia
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler icon"></span>
