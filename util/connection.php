df7d9dac59f54f8af34b23b86a1b53fc0972e1fe
maulana ibnu fajar <72007694+maulana421@users.noreply.github.com>
2022-03-25 15:19:48 +0700

    Update connection.php

--- util/connection.php
+++ util/connection.php
@@ -3,9 +3,9 @@
 session_start();
 $host = "host = john.db.elephantsql.com";
 $port = "port = 5432";
-$dbname = "dbname = faqoanct";
-$username = "user = faqoanct";
-$password = "password = g3TAi9XQDpgYe74NXMuhoZUsFkZfA0L1";
+$dbname = "dbname = ypovawlw";
+$username = "user = ypovawlw";
+$password = "password = 5awMp3gVFpO8qrWix7O__dazUTlq1czu";
 
 $connection = pg_connect("$host $port $dbname $username $password");
 if ($connection) {
